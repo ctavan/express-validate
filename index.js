4b6719e160b811b041ca7f8347ad7b67320373ec
Corpix <me@corpix.ru>
2011-06-19 19:04:33 +0300

    getParam fix

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ sanitize = require('validator').sanitize;
 
 module.exports = function(req, res, next) {
     req.getParam = function(param) {
-        return this.params[param] || (this.body ? this.body[param] : new Error('express-validator: Param %s not found', param));
+        return this.params[param] || (this.body ? this.body[param] : new Error(['express-validator: Param', param, 'not found'].join(' ')));
     }
           
     req.check = function(param, message, noSearch) {
-        return check((!noSearch ? this.getParam(param) : param), message);
+      var _param = this.getParam(param)
+      if(_param instanceof Error) throw _param
+      return check((!noSearch ? _param : param), message);
     }
           
     req.filter = req.sanitize = function(param, noSearch) { // TODO: req.filter('var').xss() instead of req.params.var = req.filter('var').xss()
-        return sanitize((!noSearch ? this.getParam(param) : param));
+      var _param = this.getParam(param)
+      if(_param instanceof Error) throw _param
+      return sanitize((!noSearch ? _param : param));
     }
     return next();
 }
