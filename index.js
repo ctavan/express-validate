44b24306199438ea1dc0b80a7f1d103565783235
Christoph Tavan <tavan@nemata.com>
2011-07-22 16:35:10 +0200

    Set type of error thrown by validator.check() to validationError

--- index.js
+++ index.js
@@ -2,33 +2,40 @@
  * Dmitry Petrov http://corpix.ru
  * http://dream-web.ru
  */
-var check = require('validator').check
-  , sanitize = require('validator').sanitize
+var Validator = require('validator').Validator;
+var v = new Validator();
+
+v.error = function(msg) {
+  var e = new Error(msg);
+  e.type = 'validationError';
+  throw e;
+    console.log('Fail');
+}
 
 module.exports = function(req, res, next) {
     req.getParam = function(param) {
         return this.params[param] || (this.query && this.query[param]) || (this.body ? this.body[param] : new Error(['express-validator: Param', param, 'not found'].join(' ')));
     }
-          
+
     req.check = function(param, message, noSearch) {
       if(!noSearch) {
         param = this.getParam(param)
         if(param instanceof Error) throw param
       }
-      var result = check(param, message)
+      var result = v.check(param, message)
       if(!result) {
         throw new Error(['express-validator:', (noSearch ? false : param) || message])
         return false
       }
       return result
     }
-          
+
     req.filter = req.sanitize = function(param, noSearch) { // TODO: req.filter('var').xss() instead of req.params.var = req.filter('var').xss()
       if(!noSearch) {
         param = this.getParam(param)
         if(param instanceof Error) throw param
       }
-      return sanitize(param)
+      return v.sanitize(param)
     }
     return next()
 }
